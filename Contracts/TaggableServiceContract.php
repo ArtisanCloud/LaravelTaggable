a072691332a842b3816b0711dbd34b9704b726d6
michaelhu <hbj148@gmail.com>
2020-09-23 00:57:05 +0800

    add extra discover

---
+++ Contracts/TaggableServiceContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ArtisanCloud\Taggable\Contracts;
+
+/**
+ * Interface TaggableServiceContract
+ * @package App\Services\TaggableService\Contracts
+ */
+interface TaggableServiceContract
+{
+    //
+}
---
+++ Providers/TaggableServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ArtisanCloud\Taggable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\TaggableService\Contracts\TaggableServiceContract;
+
+
+/**
+ * Class TaggableServiceProvider
+ * @package App\Providers
+ */
+class TaggableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            // publish config file
+
+            // register artisan command
+            if (!class_exists('CreateTaggableTable')) {
+                $this->publishes([
+                    __DIR__ . '/../../database/migrations/create_taggables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_taggables_table.php'),
+                    // you can add any number of migrations here
+                ], ['ArtisanCloud', 'Taggable-Migration']);
+            }
+        }
+    }
+}
