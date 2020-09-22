39544dfd5d5092c33acf8da0fd61f86d39bdd3da
michaelhu <hbj148@gmail.com>
2020-09-23 01:08:15 +0800

    publish taggable

--- Providers/TaggableServiceProvider.php
+++ Providers/TaggableServiceProvider.php
@@ -35,7 +35,7 @@ class TaggableServiceProvider extends ServiceProvider
             // register artisan command
             if (!class_exists('CreateTaggableTable')) {
                 $this->publishes([
-                    __DIR__ . '/../../database/migrations/create_taggables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_taggables_table.php'),
+                    __DIR__ . '/../database/migrations/create_tag_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tag_table.php'),
                     // you can add any number of migrations here
                 ], ['ArtisanCloud', 'Taggable-Migration']);
             }
